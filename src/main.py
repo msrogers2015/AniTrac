672eb07d399bee0f9b2c525dc73ca243a582c8a5
Marquel Rogers <69293041+msrogers2015@users.noreply.github.com>
2023-12-23 17:55:55 -0600

    Dashboard table created.
    Using treeview, a table to display anilox information has been created. This table has a custom theme
    that will later be updated to allow user to select certain colors as well as enable a dark mode. In
    addition, the event of switching tabs triggers the table to updated to always display up to date informaiton
    even right after milage has been inputted. An SQL class has been created to query the database to get anilox
    information from the table.

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ class App:
         # Create base GUI window
         self.root = tk.Tk()
         # Create size and location variables
-        width = 600
+        self.width = 600
         height = 400
-        x = int((self.root.winfo_screenwidth() / 2) - (width / 2))
+        x = int((self.root.winfo_screenwidth() / 2) - (self.width / 2))
         y = int((self.root.winfo_screenheight() / 2) - (height / 2))
         # Resize application window and center in screen
-        self.root.geometry(f'{width}x{height}+{x}+{y}')
+        self.root.geometry(f'{self.width}x{height}+{x}+{y}')
         # Update application window title
         self.root.title('AniTrac')
         # Create a style for the application to customize gui
@@ -31,21 +31,26 @@ class App:
         self.notebook = ttk.Notebook(self.root)
         self.notebook.pack(expand=1, fill='both')
         # Create frames to hold widgets for each tab.
-        dashboard_frame = ttk.Frame(self.notebook)
+        self.dashboard_frame = ttk.Frame(self.notebook)
         record_frame = ttk.Frame(self.notebook)
         settings_frame = ttk.Frame(self.notebook)
         # Create local instances for gui
-        dashboard_gui = dashboard.Dashboard(dashboard_frame)
+        self.dashboard_gui = dashboard.Dashboard(self.dashboard_frame, self.width)
         record_gui = record.Record(record_frame)
         settings_gui = settings.Settings(settings_frame)
         # Create tabs to navigate the different sub-windows
-        self.notebook.add(dashboard_frame, text='Dashboard')
+        self.notebook.add(self.dashboard_frame, text='Dashboard')
         self.notebook.add(record_frame, text='Record')
         self.notebook.add(settings_frame, text='Settings')
-        
-    def bindings(self):
-        pass
+        # Bind tab changing event to various functionalities. 
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_changed)
 
+    def on_tab_changed(self, event):
+        '''Actions to be taken when tab change event happens.'''
+        # If the index of the tab is one, update the table data in the
+        # dashboard
+        if self.notebook.index(self.notebook.select()) == 0:
+            self.dashboard_gui.update_table()
 
 # Run application if this file is used as the main file
 if __name__ == "__main__":
